635753d9896b6b548ce7ac72500a210232926a7c
Uktam Hudoyarov <uktamyb@gmail.com>
2021-10-12 22:14:37 +0500

    changes

--- script/cards.js
+++ script/cards.js
@@ -1,19 +1,28 @@
 let titlearray = [
-  "Кот полосатый6",
-  "Кот полосатый5",
-  "Кот полосатый4",
-  "Кот полосатый3",
-  "Кот полосатый2",
-  "Кот полосатый1"
+  "Кот полосатый",
+  "Кот пушистый",
+  "Кот необекновенный",
+  "Кот скинный",
+  "Кот барбарес",
+  "Кот вялый"
 ];
 
 let descriptionarray = [
-  "Коричневый окрас",
-  "Коричневый окрас",
-  "Коричневый окрас",
-  "Коричневый окрас",
-  "Коричневый окрас",
-  "Коричневый окрас"
+  "Коричневый <br /> окрас <span><b>2 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>",
+  "Оранжевый <br /> окрас <span><b>3 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>",
+  "Черный <br /> окрас <span><b>1,5 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>",
+  "Серый <br /> окрас <span><b>4 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>",
+  "Желтый <br /> окрас <span><b>3 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>",
+  "Красный <br /> окрас <span><b>2 мес.</b> <br /> Возраст</span> <span><b>4</b> <br /> Кол-во лап</span>"
+];
+
+let pricearray = [
+  "10 000 руб.",
+  "30 000 руб.",
+  "25 000 руб.",
+  "20 000 руб.",
+  "40 000 руб.",
+  "30 000 руб."
 ];
 
 const dynamic = document.querySelector(".main__content");
@@ -26,8 +35,8 @@ for (let i = 0; i < titlearray.length; i++) {
     <div class="card__img" id="cards${i}"></div>
     <div class="cards__content">
         <h2>${titlearray[i]}</h2>
-        <p>${descriptionarray[i]}</p>
-        <p>30 000 руб.</p>
+        <div class="card__description" >${descriptionarray[i]}</div>
+        <p style="font-weight: bold; font-size: 24px;">${pricearray[i]}</p>
     </div>
     <a class="card__buy" href="https://www.google.com/"><button class="card__buy" type="submit">
         Купить
@@ -44,15 +53,3 @@ function getSelectValue() {
   console.log(selectedValue);
 }
 getSelectValue();
-
-// Load more
-
-let cardarray = [];
-
-$(".main__content").slice(0, 3).show();
-$(".load__more").on("click", function () {
-  $(".main__content:hidden").slice(0, 3).slideDown();
-  if ($(".main__content").length == 0) {
-    $(".load__more").fadeOut("slow");
-  }
-});
